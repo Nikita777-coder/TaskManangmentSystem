6c9e6dfec116d4f63c7d312417090b3786e7acc2
Nikita777-coder <61533809+Nikita777-coder@users.noreply.github.com>
2025-01-17 13:54:16 +0300

    make opportunity to make user with different role when register

--- src/main/java/app/dto/auth/SignUpRequest.java
+++ src/main/java/app/dto/auth/SignUpRequest.java
@@ -1,5 +1,6 @@
 package app.dto.auth;
 
+import app.entity.userattributes.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -18,6 +19,8 @@ public class SignUpRequest {
     private String email;
 
     private String name = "";
+
+    private Role userRole;
 
     @NotBlank(message = "password can't be empty!")
     @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
